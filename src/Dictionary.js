43b27580158705f249944d013acecae0a8737f80
kirstyaka <78785730+kirstyaka@users.noreply.github.com>
2021-06-03 18:08:48 +0100

    Set up images API and associated autorisation

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,13 +2,19 @@ import React, { useState } from 'react';
 import "./Dictionary.css"
 import axios from 'axios';
 import Results from './Results';
+import Photos from './Photos';
 
 export default function Dictionary() {
 let [word, setWord] = useState(""); 
 let[results, setResults] = useState(null);  
+let[photos, setPhotos] = useState(null);
 
-function handleResponse(response) {
+function handleDictionaryResponse(response) {
     setResults(response.data[0]);
+}
+
+function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
 }
 
  function Search(event) {
@@ -17,7 +23,14 @@ function handleResponse(response) {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_GB/${word}`;
     //documentation: https://dictionaryapi.dev/
 
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let pexelsApiKey = "563492ad6f917000010000014980aa51a5a74ee092df4bbf37bb6ff0";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${word}&per_page=9`;
+    let headers = {"Authorization" : `Bearer ${pexelsApiKey}`};
+
+
+    axios.get(pexelsApiUrl, { headers: headers}).then(handlePexelsResponse); 
 }
 
 function handleWordChange(event) {
@@ -33,6 +46,7 @@ setWord(event.target.value);
             </form>
             </section>
             <Results results={results} />
+            <Photos photos={photos} />
         </div>
     );
 }
